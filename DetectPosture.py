0087e4ae88f7c85fab38c176709a8789fbae0e03
ng jaz winn <ngjazwinn@gmail.com>
2025-10-30 17:52:38 +0800

    pushup detector

--- DetectPosture.py
+++ DetectPosture.py
@@ -1,39 +1,126 @@
-import cv2
 import numpy as np
-
-def DetectPosture(results):
-    # straighten back
-    if len(results) == 0 or len(results[0].keypoints.xy) == 0:
-        print("No keypoints detected")
-        return
-    keypoints = results[0].keypoints.xy[0].cpu().numpy()
-    rightWrist = np.array(keypoints[10])
-    rightElbow = np.array(keypoints[8])
-    rightShoulder = np.array(keypoints[6])
-
-    ElbowToShoulder = rightElbow - rightShoulder ##for now its right elbow and right wrist
-    print( "hip to Shoulder ",ElbowToShoulder)
-
-    ElbowToWrist = rightElbow - rightWrist
-    print("hip to Ankle ", ElbowToWrist)
+import threading
+from enum import Enum
+from playsound3 import playsound
+from sympy import false
 
 
-    # Normalize vectors
-    v1 = ElbowToShoulder / np.linalg.norm(ElbowToShoulder)
-    v2 = ElbowToWrist / np.linalg.norm(ElbowToWrist)
+class Counter:
+    def __init__(self):
+        self.value = 0
 
+def PlaySoundAsync(path):
+    t = threading.Thread(target=playsound, args=(path,), daemon=True)
+    t.start()
+    return t
+
+class BodyKeyPoints(Enum):
+    NOISE = 0
+    LEFT_EYE = 1
+    RIGHT_EYE = 2
+    LEFT_EAR = 3
+    RIGHT_EAR = 4
+    LEFT_SHOULDER = 5
+    RIGHT_SHOULDER = 6
+    LEFT_ELBOW = 7
+    RIGHT_ELBOW = 8
+    LEFT_WRIST = 9
+    RIGHT_WRIST = 10
+    LEFT_HIP = 11
+    RIGHT_HIP = 12
+    LEFT_KNEE = 13
+    RIGHT_KNEE = 14
+    LEFT_ANKLE = 15
+    RIGHT_ANKLE = 16
+
+def GetAngleDegree(v1, v2):
     # Compute angle in degrees
     dot_product = np.dot(v1, v2)
     # Clamp to avoid numerical errors
     dot_product = np.clip(dot_product, -1.0, 1.0)
     angle_rad = np.arccos(dot_product)
-    angle_deg = np.degrees(angle_rad)
+    return np.degrees(angle_rad)
 
-    print("Angle between shoulder-hip and hip-ankle:", angle_deg)
+def AngleWithFloor(results, bodykeypoint1, bodykeypoint2):
+    if len(results) == 0 or len(results[0].keypoints.xy) == 0:
+        #print("No keypoints detected")
+        return
+    keypoints = results[0].keypoints.xy[0].cpu().numpy()
+
+    point_top = keypoints[bodykeypoint1.value]
+    point_bottom = keypoints[bodykeypoint2.value]
+
+    body_vec = np.array(point_top) - np.array(point_bottom)
+    dx, dy = body_vec
+    angle_deg = np.degrees(np.arctan2(dy, dx))  # angle relative to x-axis (floor)
+    return angle_deg
+
+class BodyCompression:
+
+    start = False
+
+    def __init__(self, bodykeypoint1, bodykeypoint2, bodykeypoint3):
+        self.frontPart = bodykeypoint1
+        self.middlePart = bodykeypoint2
+        self.endPart = bodykeypoint3
+
+    def GetFrontToEndVector(self):
+        return self.frontPart - self.endPart
 
 
 
-    # for result in results:
-    #     keypoints = result.keypoints  # keypoint object
-    #     print("Coordinate:", keypoints.xy)  # (x, y) coordinates
-    #     print("Confidence:", keypoints.conf)  # confidence scores per keypoint
+    def GetAngle(self, keypoints):
+        rightWrist = np.array(keypoints[self.frontPart.value])
+        rightElbow = np.array(keypoints[self.middlePart.value])
+        rightShoulder = np.array(keypoints[self.endPart.value])
+
+        ElbowToShoulder = rightElbow - rightShoulder
+
+        ElbowToWrist = rightElbow - rightWrist
+
+        # Normalize vectors
+        v1 = ElbowToShoulder / np.linalg.norm(ElbowToShoulder)
+        v2 = ElbowToWrist / np.linalg.norm(ElbowToWrist)
+
+
+        return GetAngleDegree(v1, v2)
+
+    def TestCompression(self, results, minAngle, confidence = 90):
+        if len(results) == 0 or len(results[0].keypoints.xy) == 0:
+            #print("No keypoints detected")
+            return False
+        keypoints = results[0].keypoints.xy[0].cpu().numpy()
+
+        angle_deg = self.GetAngle(keypoints)
+
+
+        if (self.start and angle_deg - confidence < minAngle):
+            self.start = False
+            return True
+            #print("Stop")
+        return False
+
+    def CheckStraightness(self, results, confidence = 20):
+        if len(results) == 0 or len(results[0].keypoints.xy) == 0:
+            #print("No keypoints detected")
+            return
+        keypoints = results[0].keypoints.xy[0].cpu().numpy()
+
+        angle_deg = self.GetAngle(keypoints)
+
+        return abs(180 - angle_deg) <= confidence
+
+    def TestCofidence(self, results, confidence = 0.5):
+        if len(results) == 0 or len(results[0].keypoints.xy) == 0:
+            #print("No keypoints detected")
+            return False
+        confs = results[0].keypoints.conf[0].cpu().numpy()  # first person
+        keypoints_to_check = {self.frontPart.value, self.middlePart.value, self.endPart.value}
+
+
+        for kp in keypoints_to_check:
+            if confs[kp] < confidence:
+                return False
+
+        return True
+
--- main.py
+++ main.py
@@ -3,20 +3,77 @@ import DetectPosture
 from ultralytics import YOLO
 print("package imported")
 
+
+counter = DetectPosture.Counter()
+rightArm = DetectPosture.BodyCompression(
+    DetectPosture.BodyKeyPoints.RIGHT_WRIST,
+    DetectPosture.BodyKeyPoints.RIGHT_ELBOW,
+    DetectPosture.BodyKeyPoints.RIGHT_SHOULDER
+)
+
+body = DetectPosture.BodyCompression(
+    DetectPosture.BodyKeyPoints.RIGHT_SHOULDER,
+    DetectPosture.BodyKeyPoints.RIGHT_HIP,
+    DetectPosture.BodyKeyPoints.RIGHT_ANKLE
+)
+
+leg = DetectPosture.BodyCompression(
+    DetectPosture.BodyKeyPoints.RIGHT_HIP,
+    DetectPosture.BodyKeyPoints.RIGHT_KNEE,
+    DetectPosture.BodyKeyPoints.RIGHT_ANKLE
+)
+
+
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640) #width
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) #height
 cap.set(10, 100)
 
-model = YOLO('Resource/Models/yolo11n-pose.pt')
+model = YOLO('Resource/Models/yolo11s-pose.pt')
 
+soundStraightenBody = None
+soundStraightenLeg = None
+soundBeep = None
+angleWithFloor = 0
 
 while True:
     success, img = cap.read()
-    results = model(img)
-    DetectPosture.DetectPosture(results)
-    annotated_frame = results[0].plot()  # Draw boxes and labels
+    results = results = model(img)
 
+    textColor = (255, 255, 255)
+    if(rightArm.CheckStraightness(results, 30)):
+        rightArm.start = True
+        textColor = (0, 255, 0)
+    else:
+        textColor = (0, 0, 255)
+
+
+
+    if(rightArm.TestCofidence(results) and rightArm.TestCompression(results, 40)):
+        print("first stage pass!")
+        # angleWithFloor = DetectPosture.AngleWithFloor(results, DetectPosture.BodyKeyPoints.RIGHT_SHOULDER,
+        #                                               DetectPosture.BodyKeyPoints.RIGHT_ANKLE)
+        #
+        #
+        # print("Angle between floor and object is: ", angleWithFloor)
+        # if(angleWithFloor < 40):
+
+        if not (body.CheckStraightness(results)):
+            if (soundStraightenBody is None or not soundStraightenBody.is_alive()):
+                soundStraightenBody = DetectPosture.PlaySoundAsync("Resource/Sound/straigthenbody.mp3")
+        elif not (leg.CheckStraightness(results, 80)):
+            if (soundStraightenLeg is None or not soundStraightenLeg.is_alive()):
+                soundStraightenLeg = DetectPosture.PlaySoundAsync("Resource/Sound/straigthenbody.mp3")
+        else:
+            counter.value += 1
+            if (soundBeep is None or not soundBeep.is_alive()):
+                soundBeep = DetectPosture.PlaySoundAsync("Resource/Sound/beep.mp3")
+
+
+
+    annotated_frame = results[0].plot()  # Draw boxes and labels
+    #cv2.putText(annotated_frame, str(angleWithFloor), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1, textColor, 2)
+    cv2.putText(annotated_frame, str(counter.value), (50,50), cv2.FONT_HERSHEY_PLAIN, 2, textColor, 2)
     cv2.imshow("Video", annotated_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
